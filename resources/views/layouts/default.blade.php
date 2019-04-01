c8f5c533693f1c3ff757d6cbf01e6d3af5ad81d1
Tatianna Torres <tat8@njit.edu>
2019-04-01 02:03:31 -0400

    Added files in layout,pages and includes

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+    @include('includes.menu')
+
+    <main role="main">
+        @yield('content')
+    </main>
+
+@include('includes.jsfooter')
+@include('includes.footer')
+
+</body>
+</html>
